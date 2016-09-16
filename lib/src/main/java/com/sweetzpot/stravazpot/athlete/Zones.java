6eb8d170c5da45e4e7218f28e55819e0fa310d89
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-16 15:16:57 +0200

    Create class to represent athlete zones

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/athlete/Zones.java
@@ -0,0 +1,14 @@
+package com.sweetzpot.stravazpot.athlete;
+
+public class Zones {
+    private HeartRate heartRate;
+    private Power power;
+
+    public HeartRate getHeartRate() {
+        return heartRate;
+    }
+
+    public Power getPower() {
+        return power;
+    }
+}

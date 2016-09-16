a51f010e0fefbdc69ce035cf9f3dda75c53506e0
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-16 15:13:40 +0200

    Create class to represent intervals

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/Interval.java
@@ -0,0 +1,14 @@
+package com.sweetzpot.stravazpot;
+
+public class Interval<T> {
+    private T min;
+    private T max;
+
+    public T getMin() {
+        return min;
+    }
+
+    public T getMax() {
+        return max;
+    }
+}

05b456b89d90f3c880a85843510ec3365e7047af
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-16 15:14:58 +0200

    Create class to represent heart rate zones

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/athlete/HeartRate.java
@@ -0,0 +1,18 @@
+package com.sweetzpot.stravazpot.athlete;
+
+import com.sweetzpot.stravazpot.Interval;
+
+import java.util.List;
+
+public class HeartRate {
+    private boolean customZones;
+    private List<Interval<Float>> zones;
+
+    public boolean hasCustomZones() {
+        return customZones;
+    }
+
+    public List<Interval<Float>> getZones() {
+        return zones;
+    }
+}

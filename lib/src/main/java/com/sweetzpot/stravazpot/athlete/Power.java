dc075199d55b1139c2f8d991f00261008999b77d
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-16 15:16:03 +0200

    Create class to represent power zones

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/athlete/Power.java
@@ -0,0 +1,13 @@
+package com.sweetzpot.stravazpot.athlete;
+
+import com.sweetzpot.stravazpot.Interval;
+
+import java.util.List;
+
+public class Power {
+    private List<Interval<Float>> zones;
+
+    public List<Interval<Float>> getZones() {
+        return zones;
+    }
+}

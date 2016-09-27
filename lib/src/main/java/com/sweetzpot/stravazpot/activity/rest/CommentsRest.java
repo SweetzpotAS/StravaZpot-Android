7e4e2e1a31d0e74dd9fc979b256786c97a9c2c48
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 16:31:33 +0200

    Create interface to interact with comments REST API

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/rest/CommentsRest.java
@@ -0,0 +1,18 @@
+package com.sweetzpot.stravazpot.activity.rest;
+
+import com.sweetzpot.stravazpot.activity.model.Comment;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface CommentsRest {
+    @GET("activities/{id}/comments")
+    Call<List<Comment>> getActivityComments(
+            @Path("id") Integer id,
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+}

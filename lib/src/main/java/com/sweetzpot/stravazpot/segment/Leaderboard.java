f6094f9d4cf7bef90e5a398e2b97c8a1b392a275
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 11:05:27 +0200

    Create class to represent leaderboards

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/segment/Leaderboard.java
@@ -0,0 +1,16 @@
+package com.sweetzpot.stravazpot.segment;
+
+import java.util.List;
+
+public class Leaderboard {
+    private int entryCount;
+    private List<LeaderboardEntry> entries;
+
+    public int getEntryCount() {
+        return entryCount;
+    }
+
+    public List<LeaderboardEntry> getEntries() {
+        return entries;
+    }
+}

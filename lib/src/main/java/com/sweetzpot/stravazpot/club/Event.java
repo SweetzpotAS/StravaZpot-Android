510bc7b4b51a3976fbc9f3468147a6aaba1074a2
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 10:37:50 +0200

    Create class to represent events

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/club/Event.java
@@ -0,0 +1,86 @@
+package com.sweetzpot.stravazpot.club;
+
+import com.sweetzpot.stravazpot.ResourceState;
+import com.sweetzpot.stravazpot.activity.ActivityType;
+import com.sweetzpot.stravazpot.athlete.Athlete;
+
+import java.util.Date;
+import java.util.List;
+
+public class Event {
+    private int ID;
+    private ResourceState resourceState;
+    private String title;
+    private String description;
+    private int clubID;
+    private Athlete organizingAthlete;
+    private ActivityType activityType;
+    private Date createdAt;
+    private int routeID;
+    private boolean womanOnly;
+    private boolean isPrivate;
+    private SkillLevel skillLevel;
+    private Terrain terrain;
+    private List<Date> upcomingOccurrences;
+    private String address;
+
+    public int getID() {
+        return ID;
+    }
+
+    public ResourceState getResourceState() {
+        return resourceState;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getClubID() {
+        return clubID;
+    }
+
+    public Athlete getOrganizingAthlete() {
+        return organizingAthlete;
+    }
+
+    public ActivityType getActivityType() {
+        return activityType;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getRouteID() {
+        return routeID;
+    }
+
+    public boolean isWomanOnly() {
+        return womanOnly;
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public SkillLevel getSkillLevel() {
+        return skillLevel;
+    }
+
+    public Terrain getTerrain() {
+        return terrain;
+    }
+
+    public List<Date> getUpcomingOccurrences() {
+        return upcomingOccurrences;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}

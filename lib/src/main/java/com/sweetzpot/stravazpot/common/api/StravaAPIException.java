2bfaf6ddda30b0871403dd004c12578d1bbf560c
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 18:04:57 +0200

    Create exception to represent errors contacting the API

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/api/StravaAPIException.java
@@ -0,0 +1,18 @@
+package com.sweetzpot.stravazpot.common.api;
+
+public class StravaAPIException extends RuntimeException{
+    public StravaAPIException() {
+    }
+
+    public StravaAPIException(String message) {
+        super(message);
+    }
+
+    public StravaAPIException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StravaAPIException(Throwable cause) {
+        super(cause);
+    }
+}

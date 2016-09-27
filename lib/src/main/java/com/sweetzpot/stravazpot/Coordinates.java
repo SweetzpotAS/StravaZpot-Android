cd1c0df5381434a2778e11f1210fd2c5d682f66f
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 10:13:20 +0200

    Create class to represent coordinates

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/Coordinates.java
@@ -0,0 +1,14 @@
+package com.sweetzpot.stravazpot;
+
+public class Coordinates {
+    private float latitude;
+    private float longitude;
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+}

cbe1e722a8cf0193d0941326d8652ff0bcf0ab55
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 17:13:54 +0200

    Create interface to interact with segment REST API

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/segment/rest/SegmentRest.java
@@ -0,0 +1,71 @@
+package com.sweetzpot.stravazpot.segment.rest;
+
+import com.sweetzpot.stravazpot.activity.model.ActivityType;
+import com.sweetzpot.stravazpot.common.model.Gender;
+import com.sweetzpot.stravazpot.segment.model.AgeGroup;
+import com.sweetzpot.stravazpot.segment.model.DateRange;
+import com.sweetzpot.stravazpot.segment.model.Leaderboard;
+import com.sweetzpot.stravazpot.segment.model.Segment;
+import com.sweetzpot.stravazpot.segment.model.SegmentEffort;
+import com.sweetzpot.stravazpot.segment.model.WeightClass;
+
+import java.util.Date;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface SegmentRest {
+    @GET("segments/{id}")
+    Call<Segment> getSegment(@Path("id") Integer id);
+
+    @GET("segments/starred")
+    Call<List<Segment>> getMyStarredSegments(
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @GET("athletes/{id}/segments/starred")
+    Call<List<Segment>> getAthleteStarredSegments(
+            @Path("id") Integer id,
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @PUT("segments/{id}/starred")
+    Call<Segment> starSegment(
+            @Path("id") Integer id,
+            @Field("starred") Boolean starred);
+
+    @GET("segments/{id}/all_efforts")
+    Call<List<SegmentEffort>> getSegmentEfforts(
+            @Path("id") Integer id,
+            @Query("athlete_id") Integer athleteID,
+            @Query("start_date_local") Date startDate,
+            @Query("end_date_local") Date endDate,
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @GET("segments/{id}/leaderboard")
+    Call<Leaderboard> getSegmentLeaderboard(
+            @Path("id") Long id,
+            @Query("gender") Gender gender,
+            @Query("age_group") AgeGroup ageGroup,
+            @Query("weight_class") WeightClass weightClass,
+            @Query("following") Boolean following,
+            @Query("club_id") Integer clubID,
+            @Query("date_rage") DateRange dateRange,
+            @Query("context_entries") Integer contextEntries,
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @GET("segments/explore")
+    Call<List<Segment>> exploreSegments(
+            @Query("bounds") String bounds,
+            @Query("activity_type") ActivityType activityType,
+            @Query("min_cat") Integer minCat,
+            @Query("max_cat") Integer maxCat);
+
+}

71806927d1a980409731fb293a0c0cfded4898ea
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 10:32:58 +0200

    Create class to represent announcements

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/club/Announcement.java
@@ -0,0 +1,39 @@
+package com.sweetzpot.stravazpot.club;
+
+import com.sweetzpot.stravazpot.ResourceState;
+import com.sweetzpot.stravazpot.athlete.Athlete;
+
+import java.util.Date;
+
+public class Announcement {
+    private int ID;
+    private ResourceState resourceState;
+    private int clubID;
+    private Athlete athlete;
+    private Date createdAt;
+    private String message;
+
+    public int getID() {
+        return ID;
+    }
+
+    public ResourceState getResourceState() {
+        return resourceState;
+    }
+
+    public int getClubID() {
+        return clubID;
+    }
+
+    public Athlete getAthlete() {
+        return athlete;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

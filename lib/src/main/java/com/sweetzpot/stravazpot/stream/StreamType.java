f3eb54c28c11947d3fa4e057a8b62c568bc3a14c
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 11:13:04 +0200

    Create enum to represent stream types

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/stream/StreamType.java
@@ -0,0 +1,15 @@
+package com.sweetzpot.stravazpot.stream;
+
+public enum StreamType {
+    TIME,
+    LATLNG,
+    DISTANCE,
+    ALTITUDE,
+    VELOCITY_SMOOTH,
+    HEART_RATE,
+    CADENCE,
+    WATTS,
+    TEMPERATURE,
+    MOVING,
+    GRADE_SMOOTH
+}

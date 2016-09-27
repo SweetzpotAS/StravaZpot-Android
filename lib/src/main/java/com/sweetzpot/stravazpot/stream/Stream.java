5848ad8a9c5eb9cdeed523a7d5aa031a7e3fee1b
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 13:21:04 +0200

    Annotate fields in stream class

--- lib/src/main/java/com/sweetzpot/stravazpot/stream/Stream.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/stream/Stream.java
@@ -1,13 +1,15 @@
 package com.sweetzpot.stravazpot.stream;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class Stream {
-    private StreamType type;
-    private List<Object> data;
-    private String seriesType;
-    private int originalSize;
-    private Resolution resolution;
+    @SerializedName("type") private StreamType type;
+    @SerializedName("data") private List<Object> data;
+    @SerializedName("series_type") private String seriesType;
+    @SerializedName("original_size") private int originalSize;
+    @SerializedName("resolution") private Resolution resolution;
 
     public StreamType getType() {
         return type;

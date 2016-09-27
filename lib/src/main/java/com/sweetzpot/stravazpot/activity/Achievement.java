13c05b0d75fe1953cdb8d5952ddd5d8b5171208e
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 10:18:16 +0200

    Create class to represent achievement

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/Achievement.java
@@ -0,0 +1,19 @@
+package com.sweetzpot.stravazpot.activity;
+
+public class Achievement {
+    private AchievementType typeID;
+    private String type;
+    private int rank;
+
+    public AchievementType getTypeID() {
+        return typeID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+}

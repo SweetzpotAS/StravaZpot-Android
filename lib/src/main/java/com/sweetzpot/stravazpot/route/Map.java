ab97fece888a9d1f642a0da64483eee29c4040a0
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 13:12:58 +0200

    Annotate fields in map class

--- lib/src/main/java/com/sweetzpot/stravazpot/route/Map.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/route/Map.java
@@ -1,11 +1,12 @@
 package com.sweetzpot.stravazpot.route;
 
+import com.google.gson.annotations.SerializedName;
 import com.sweetzpot.stravazpot.common.ResourceState;
 
 public class Map {
-    private int ID;
-    private ResourceState resourceState;
-    private String summaryPolyline;
+    @SerializedName("id") private int ID;
+    @SerializedName("resource_state") private ResourceState resourceState;
+    @SerializedName("summary_polyline") private String summaryPolyline;
 
     public int getID() {
         return ID;

cebad5c292da00820e7e3354417e55544a88f4b4
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 09:54:17 +0200

    Create class to represent athlete statistics

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/athlete/Stats.java
@@ -0,0 +1,61 @@
+package com.sweetzpot.stravazpot.athlete;
+
+import com.sweetzpot.stravazpot.Distance;
+
+public class Stats {
+    private Distance biggestRideDistance;
+    private Distance biggestClimbElevationGain;
+    private Totals recentRideTotals;
+    private Totals recentRunTotals;
+    private Totals recentSwimTotals;
+    private Totals yearToDateRideTotals;
+    private Totals yearToDateRunTotals;
+    private Totals yearToDateSwimTotals;
+    private Totals allRideTotals;
+    private Totals allRunTotals;
+    private Totals allSwimTotals;
+
+    public Distance getBiggestRideDistance() {
+        return biggestRideDistance;
+    }
+
+    public Distance getBiggestClimbElevationGain() {
+        return biggestClimbElevationGain;
+    }
+
+    public Totals getRecentRideTotals() {
+        return recentRideTotals;
+    }
+
+    public Totals getRecentRunTotals() {
+        return recentRunTotals;
+    }
+
+    public Totals getRecentSwimTotals() {
+        return recentSwimTotals;
+    }
+
+    public Totals getYearToDateRideTotals() {
+        return yearToDateRideTotals;
+    }
+
+    public Totals getYearToDateRunTotals() {
+        return yearToDateRunTotals;
+    }
+
+    public Totals getYearToDateSwimTotals() {
+        return yearToDateSwimTotals;
+    }
+
+    public Totals getAllRideTotals() {
+        return allRideTotals;
+    }
+
+    public Totals getAllRunTotals() {
+        return allRunTotals;
+    }
+
+    public Totals getAllSwimTotals() {
+        return allSwimTotals;
+    }
+}

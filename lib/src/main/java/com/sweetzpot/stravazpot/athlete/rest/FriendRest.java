ce4e34fca7dafbc25b92a8179974c569f36a5d39
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 13:49:29 +0200

    Create interface to interact with friends REST API

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/athlete/rest/FriendRest.java
@@ -0,0 +1,40 @@
+package com.sweetzpot.stravazpot.athlete.rest;
+
+import com.sweetzpot.stravazpot.athlete.model.Athlete;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface FriendRest {
+    @GET("athlete/friends")
+    Call<List<Athlete>> getMyFriends(
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @GET("athletes/{id}/friends")
+    Call<List<Athlete>> getFriends(
+            @Path("id") Integer id,
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @GET("athlete/followers")
+    Call<List<Athlete>> getMyFollowers(
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @GET("athletes/{id}/followers")
+    Call<List<Athlete>> getFollowers(
+            @Path("id") Integer id,
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+
+    @GET("athletes/{id}/both-following")
+    Call<List<Athlete>> getBothFollowing(
+            @Path("id") Integer id,
+            @Query("page") Integer page,
+            @Query("per_page") Integer perPage);
+}

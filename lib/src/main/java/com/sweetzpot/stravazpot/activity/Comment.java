ab8e499e48647662bae12b6c471d408aa24468b4
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 10:23:41 +0200

    Create class to represent comments

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/Comment.java
@@ -0,0 +1,39 @@
+package com.sweetzpot.stravazpot.activity;
+
+import com.sweetzpot.stravazpot.ResourceState;
+import com.sweetzpot.stravazpot.athlete.Athlete;
+
+import java.util.Date;
+
+public class Comment {
+    private int id;
+    private ResourceState resourceState;
+    private int activityID;
+    private String text;
+    private Athlete athlete;
+    private Date createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public ResourceState getResourceState() {
+        return resourceState;
+    }
+
+    public int getActivityID() {
+        return activityID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Athlete getAthlete() {
+        return athlete;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}

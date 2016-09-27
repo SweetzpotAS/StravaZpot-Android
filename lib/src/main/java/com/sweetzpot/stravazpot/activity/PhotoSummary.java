75b75c217f1b471472daaba66bc2d116e543be46
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 10:09:52 +0200

    Create class to represent summary of photos

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/PhotoSummary.java
@@ -0,0 +1,14 @@
+package com.sweetzpot.stravazpot.activity;
+
+public class PhotoSummary {
+    private int count;
+    private PrimaryPhoto primaryPhoto;
+
+    public int getCount() {
+        return count;
+    }
+
+    public PrimaryPhoto getPrimaryPhoto() {
+        return primaryPhoto;
+    }
+}

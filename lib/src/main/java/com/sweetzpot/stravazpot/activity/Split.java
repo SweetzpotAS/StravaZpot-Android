6fe2f0daf7981c1a16780583a74473953c8f44fb
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 13:05:30 +0200

    Annotate fields in split class

--- lib/src/main/java/com/sweetzpot/stravazpot/activity/Split.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/Split.java
@@ -1,14 +1,15 @@
 package com.sweetzpot.stravazpot.activity;
 
+import com.google.gson.annotations.SerializedName;
 import com.sweetzpot.stravazpot.common.Distance;
 import com.sweetzpot.stravazpot.common.Time;
 
 public class Split {
-    private Distance distance;
-    private Time elapsedTime;
-    private Distance elevationDifference;
-    private Time movingTime;
-    private int split;
+    @SerializedName("distance") private Distance distance;
+    @SerializedName("elapsed_time") private Time elapsedTime;
+    @SerializedName("elevation_difference") private Distance elevationDifference;
+    @SerializedName("moving_time") private Time movingTime;
+    @SerializedName("split") private int split;
 
     public Distance getDistance() {
         return distance;

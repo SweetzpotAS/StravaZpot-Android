9d9fe67cb17481facaf75527717b6d77b147836e
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 13:16:26 +0200

    Annotate fields in leaderboard class

--- lib/src/main/java/com/sweetzpot/stravazpot/segment/Leaderboard.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/segment/Leaderboard.java
@@ -1,10 +1,12 @@
 package com.sweetzpot.stravazpot.segment;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class Leaderboard {
-    private int entryCount;
-    private List<LeaderboardEntry> entries;
+    @SerializedName("entry_count") private int entryCount;
+    @SerializedName("entries") private List<LeaderboardEntry> entries;
 
     public int getEntryCount() {
         return entryCount;

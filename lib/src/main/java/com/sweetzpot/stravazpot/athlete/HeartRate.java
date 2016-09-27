38b4e07009c06598faad2090f37a5f9731b080c2
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-27 12:39:06 +0200

    Annotate fields in heart rate class

--- lib/src/main/java/com/sweetzpot/stravazpot/athlete/HeartRate.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/athlete/HeartRate.java
@@ -1,12 +1,13 @@
 package com.sweetzpot.stravazpot.athlete;
 
+import com.google.gson.annotations.SerializedName;
 import com.sweetzpot.stravazpot.common.Interval;
 
 import java.util.List;
 
 public class HeartRate {
-    private boolean customZones;
-    private List<Interval<Float>> zones;
+    @SerializedName("custom_zones") private boolean customZones;
+    @SerializedName("zones") private List<Interval<Float>> zones;
 
     public boolean hasCustomZones() {
         return customZones;

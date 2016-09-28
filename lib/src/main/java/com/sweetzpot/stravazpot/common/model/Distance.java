89717ac779757cbcd05ee4991b7c6326abc1b8f0
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-28 10:40:46 +0200

    Create distance type adapter

--- lib/src/main/java/com/sweetzpot/stravazpot/common/model/Distance.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/model/Distance.java
@@ -3,6 +3,10 @@ package com.sweetzpot.stravazpot.common.model;
 public class Distance {
     private float meters;
 
+    public Distance(float meters) {
+        this.meters = meters;
+    }
+
     public float getMeters() {
         return meters;
     }
---
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/DistanceTypeAdapter.java
@@ -0,0 +1,21 @@
+package com.sweetzpot.stravazpot.common.typeadapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.sweetzpot.stravazpot.common.model.Distance;
+
+import java.io.IOException;
+
+public class DistanceTypeAdapter extends TypeAdapter<Distance> {
+
+    @Override
+    public void write(JsonWriter out, Distance value) throws IOException {
+        out.value(value.getMeters());
+    }
+
+    @Override
+    public Distance read(JsonReader in) throws IOException {
+        return new Distance((float) (in.nextDouble()));
+    }
+}

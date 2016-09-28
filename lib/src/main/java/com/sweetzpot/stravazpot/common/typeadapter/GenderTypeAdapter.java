b7a4e697773cd3e12a936398c7538c351b085ab2
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-28 11:04:33 +0200

    Fix null value problem in gender type adapter

--- lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/GenderTypeAdapter.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/GenderTypeAdapter.java
@@ -2,6 +2,7 @@ package com.sweetzpot.stravazpot.common.typeadapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.sweetzpot.stravazpot.common.model.Gender;
 
@@ -27,15 +28,17 @@ public class GenderTypeAdapter extends TypeAdapter<Gender> {
 
     @Override
     public Gender read(JsonReader in) throws IOException {
-        String value = in.nextString();
-
-        if(value == null) {
-            return Gender.NOT_DEFINED;
-        } else if(value.equalsIgnoreCase("M")) {
-            return Gender.MALE;
-        } else if(value.equalsIgnoreCase("F")) {
-            return Gender.FEMALE;
+        if(!in.peek().equals(JsonToken.NULL)) {
+            String value = in.nextString();
+            if (value.equalsIgnoreCase("M")) {
+                return Gender.MALE;
+            } else if (value.equalsIgnoreCase("F")) {
+                return Gender.FEMALE;
+            }
+        } else {
+            in.nextNull();
         }
+
         return Gender.NOT_DEFINED;
     }
 }

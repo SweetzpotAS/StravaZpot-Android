551cb7ed61c07a505bf98fc3ecffaa1844cb0485
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-29 12:00:08 +0200

    Create type adapter for activity type

--- lib/src/main/java/com/sweetzpot/stravazpot/activity/model/ActivityType.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/model/ActivityType.java
@@ -1,33 +1,44 @@
 package com.sweetzpot.stravazpot.activity.model;
 
 public enum ActivityType {
-    RIDE,
-    RUN,
-    SWIM,
-    HIKE,
-    WALK,
-    ALPINE_SKI,
-    BACKCOUNTRY_SKI,
-    CANOEING,
-    CROSSFIT,
-    EBIKE_RIDE,
-    ELLIPTICAL,
-    ICE_SKATE,
-    INLINE_SKATE,
-    KAYAKING,
-    KITE_SURF,
-    NORDIC_SKI,
-    ROCK_CLIMBING,
-    ROLLER_SKI,
-    ROWING,
-    SNOWBOARD,
-    SNOWSHOE,
-    STAIR_STEPPER,
-    STANDUP_PADDLING,
-    SURFING,
-    VIRTUAL_RIDE,
-    WEIGHT_TRAINING,
-    WINDSURF,
-    WORKOUT,
-    YOGA
+    RIDE("Ride"),
+    RUN("Run"),
+    SWIM("Swim"),
+    HIKE("Hike"),
+    WALK("Walk"),
+    ALPINE_SKI("AlpineSki"),
+    BACKCOUNTRY_SKI("BackcountrySki"),
+    CANOEING("Canoeing"),
+    CROSSFIT("Crossfit"),
+    EBIKE_RIDE("EBikeRide"),
+    ELLIPTICAL("Elliptical"),
+    ICE_SKATE("IceSkate"),
+    INLINE_SKATE("InlineSkate"),
+    KAYAKING("Kayaking"),
+    KITE_SURF("Kitesurf"),
+    NORDIC_SKI("NordicSki"),
+    ROCK_CLIMBING("RockClimbing"),
+    ROLLER_SKI("RollerSki"),
+    ROWING("Rowing"),
+    SNOWBOARD("Snowboard"),
+    SNOWSHOE("Snowshoe"),
+    STAIR_STEPPER("StairStepper"),
+    STANDUP_PADDLING("StandUpPaddling"),
+    SURFING("Surfing"),
+    VIRTUAL_RIDE("VirtualRide"),
+    WEIGHT_TRAINING("WeightTraining"),
+    WINDSURF("Windsurf"),
+    WORKOUT("Workout"),
+    YOGA("Yoga");
+
+    private String rawValue;
+
+    ActivityType(String rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    @Override
+    public String toString() {
+        return rawValue;
+    }
 }
---
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/ActivityTypeTypeAdapter.java
@@ -0,0 +1,27 @@
+package com.sweetzpot.stravazpot.common.typeadapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.sweetzpot.stravazpot.activity.model.ActivityType;
+
+import java.io.IOException;
+
+public class ActivityTypeTypeAdapter extends TypeAdapter<ActivityType> {
+
+    @Override
+    public void write(JsonWriter out, ActivityType value) throws IOException {
+        out.value(value.toString());
+    }
+
+    @Override
+    public ActivityType read(JsonReader in) throws IOException {
+        String input = in.nextString();
+        for(ActivityType type : ActivityType.values()) {
+            if(type.toString().equalsIgnoreCase(input)) {
+                return type;
+            }
+        }
+        return ActivityType.RIDE;
+    }
+}

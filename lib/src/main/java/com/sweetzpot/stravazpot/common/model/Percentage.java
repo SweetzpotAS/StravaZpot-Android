7c2d8d46a2c73edd36f487c07ff001f9a35ac835
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-29 12:01:52 +0200

    Implement equals in percentage

--- lib/src/main/java/com/sweetzpot/stravazpot/common/model/Percentage.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/model/Percentage.java
@@ -14,4 +14,20 @@ public class Percentage {
     public float getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Percentage that = (Percentage) o;
+
+        return Float.compare(that.value, value) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (value != +0.0f ? Float.floatToIntBits(value) : 0);
+    }
 }

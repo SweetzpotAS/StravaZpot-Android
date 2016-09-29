8c50f88bc646648cd4f135a6a91f381e28de7322
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-29 12:04:31 +0200

    Implement equals in coordinates

--- lib/src/main/java/com/sweetzpot/stravazpot/common/model/Coordinates.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/model/Coordinates.java
@@ -20,4 +20,23 @@ public class Coordinates {
     public float getLongitude() {
         return longitude;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinates that = (Coordinates) o;
+
+        if (Float.compare(that.latitude, latitude) != 0) return false;
+        return Float.compare(that.longitude, longitude) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (latitude != +0.0f ? Float.floatToIntBits(latitude) : 0);
+        result = 31 * result + (longitude != +0.0f ? Float.floatToIntBits(longitude) : 0);
+        return result;
+    }
 }

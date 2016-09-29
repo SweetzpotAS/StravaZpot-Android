c367b325d68215a402b1a7171e5e9119d5555271
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-29 18:49:38 +0200

    Add raw value to stream type

--- lib/src/main/java/com/sweetzpot/stravazpot/stream/model/StreamType.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/stream/model/StreamType.java
@@ -1,15 +1,26 @@
 package com.sweetzpot.stravazpot.stream.model;
 
 public enum StreamType {
-    TIME,
-    LATLNG,
-    DISTANCE,
-    ALTITUDE,
-    VELOCITY_SMOOTH,
-    HEART_RATE,
-    CADENCE,
-    WATTS,
-    TEMPERATURE,
-    MOVING,
-    GRADE_SMOOTH
+    TIME("time"),
+    LATLNG("latlng"),
+    DISTANCE("distance"),
+    ALTITUDE("altitude"),
+    VELOCITY_SMOOTH("velocity_smooth"),
+    HEART_RATE("heartrate"),
+    CADENCE("cadence"),
+    WATTS("watts"),
+    TEMPERATURE("temp"),
+    MOVING("moving"),
+    GRADE_SMOOTH("grade_smooth");
+
+    private String rawValue;
+
+    StreamType(String rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    @Override
+    public String toString() {
+        return rawValue;
+    }
 }

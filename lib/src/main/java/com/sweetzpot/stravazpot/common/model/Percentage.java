4c7908d2a51a06b93258f5694aef03a1b3670a01
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-29 11:48:26 +0200

    Create percentage type adapter

--- lib/src/main/java/com/sweetzpot/stravazpot/common/model/Percentage.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/model/Percentage.java
@@ -3,6 +3,14 @@ package com.sweetzpot.stravazpot.common.model;
 public class Percentage {
     private float value;
 
+    public static Percentage of(float value) {
+        return new Percentage(value);
+    }
+
+    public Percentage(float value) {
+        this.value = value;
+    }
+
     public float getValue() {
         return value;
     }
---
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/PercentageTypeAdapter.java
@@ -0,0 +1,21 @@
+package com.sweetzpot.stravazpot.common.typeadapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.sweetzpot.stravazpot.common.model.Percentage;
+
+import java.io.IOException;
+
+public class PercentageTypeAdapter extends TypeAdapter<Percentage> {
+
+    @Override
+    public void write(JsonWriter out, Percentage percentage) throws IOException {
+        out.value(percentage.getValue());
+    }
+
+    @Override
+    public Percentage read(JsonReader in) throws IOException {
+        return Percentage.of((float) (in.nextDouble()));
+    }
+}

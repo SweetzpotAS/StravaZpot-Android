d34673d9e88d7d654fe9d041c3129ed6e64c0387
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-29 11:52:25 +0200

    Create type adapter for coordinates

--- lib/src/main/java/com/sweetzpot/stravazpot/common/model/Coordinates.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/model/Coordinates.java
@@ -4,6 +4,15 @@ public class Coordinates {
     private float latitude;
     private float longitude;
 
+    public static Coordinates at(float latitude, float longitude) {
+        return new Coordinates(latitude, longitude);
+    }
+
+    public Coordinates(float latitude, float longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
     public float getLatitude() {
         return latitude;
     }

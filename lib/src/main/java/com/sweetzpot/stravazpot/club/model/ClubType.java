2f985f15cd336adff720fcff78bc477e2f0fab78
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-30 10:16:16 +0200

    Create type adapter for club type

--- lib/src/main/java/com/sweetzpot/stravazpot/club/model/ClubType.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/club/model/ClubType.java
@@ -1,9 +1,20 @@
 package com.sweetzpot.stravazpot.club.model;
 
 public enum ClubType {
-    CASUAL_CLUB,
-    RACING_TEAM,
-    SHOP,
-    COMPANY,
-    OTHER
+    CASUAL_CLUB("casual_club"),
+    RACING_TEAM("racing_team"),
+    SHOP("shop"),
+    COMPANY("company"),
+    OTHER("other");
+
+    private String rawValue;
+
+    ClubType(String rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    @Override
+    public String toString() {
+        return rawValue;
+    }
 }
---
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/ClubTypeTypeAdapter.java
@@ -0,0 +1,28 @@
+package com.sweetzpot.stravazpot.common.typeadapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.sweetzpot.stravazpot.club.model.ClubType;
+
+import java.io.IOException;
+
+public class ClubTypeTypeAdapter extends TypeAdapter<ClubType> {
+
+    @Override
+    public void write(JsonWriter out, ClubType type) throws IOException {
+        out.value(type.toString());
+    }
+
+    @Override
+    public ClubType read(JsonReader in) throws IOException {
+        String input = in.nextString();
+        for(ClubType type : ClubType.values()) {
+            if(type.toString().equalsIgnoreCase(input)) {
+                return type;
+            }
+        }
+
+        return null;
+    }
+}

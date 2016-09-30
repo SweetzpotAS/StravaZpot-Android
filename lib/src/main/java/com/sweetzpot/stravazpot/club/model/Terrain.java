8721857fda3a7d08ae82d12aa3b0fb0fc1986cb7
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-30 11:06:47 +0200

    Create type adapter for terrain

--- lib/src/main/java/com/sweetzpot/stravazpot/club/model/Terrain.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/club/model/Terrain.java
@@ -1,7 +1,17 @@
 package com.sweetzpot.stravazpot.club.model;
 
 public enum Terrain {
-    MOSTLY_FLAT,
-    ROLLING_HILLS,
-    KILLER_CLIMBS
+    MOSTLY_FLAT(0),
+    ROLLING_HILLS(1),
+    KILLER_CLIMBS(2);
+
+    private int rawValue;
+
+    Terrain(int rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    public int getRawValue() {
+        return rawValue;
+    }
 }

6fd27503b1758e636b2af679b427cf716bb72b31
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-30 10:25:33 +0200

    Create type adapter for membership

--- lib/src/main/java/com/sweetzpot/stravazpot/club/model/Membership.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/club/model/Membership.java
@@ -1,7 +1,18 @@
 package com.sweetzpot.stravazpot.club.model;
 
 public enum Membership {
-    MEMBER,
-    PENDING,
-    NOT_MEMBER
+    MEMBER("member"),
+    PENDING("pending"),
+    NOT_MEMBER("null");
+
+    private String rawType;
+
+    Membership(String rawType) {
+        this.rawType = rawType;
+    }
+
+    @Override
+    public String toString() {
+        return rawType;
+    }
 }

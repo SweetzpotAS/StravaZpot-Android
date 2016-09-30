9f94fb311dccead0282e7101e760ad796575409e
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-09-30 12:29:55 +0200

    Create request to list activity kudoers

---
+++ lib/src/test/java/com/sweetzpot/stravazpot/activity/api/KudosAPITest.java
@@ -0,0 +1,32 @@
+package com.sweetzpot.stravazpot.activity.api;
+
+import com.sweetzpot.stravazpot.athlete.model.Athlete;
+import com.sweetzpot.stravazpot.common.api.StravaAPITest;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class KudosAPITest extends StravaAPITest {
+
+    @Test
+    public void shouldListActivityKudoers() throws Exception {
+        enqueueResponse("[]");
+        KudosAPI kudosAPI = givenAKudosAPI();
+
+        List<Athlete> athletes = kudosAPI.listActivityKudoers(123456)
+                                            .inPage(2)
+                                            .perPage(10)
+                                            .execute();
+
+        assertRequestPathContains(
+                "/activities/123456/kudos",
+                "page=2",
+                "per_page=10"
+        );
+    }
+
+    private KudosAPI givenAKudosAPI() {
+        return new KudosAPI(givenAValidConfig());
+    }
+}

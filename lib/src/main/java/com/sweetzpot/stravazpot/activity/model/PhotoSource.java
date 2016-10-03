d61f8403110e683ad75bbb328c648598bb91cf4d
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-03 11:11:02 +0200

    Create type adapter for photo source

--- lib/src/main/java/com/sweetzpot/stravazpot/activity/model/PhotoSource.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/model/PhotoSource.java
@@ -1,6 +1,21 @@
 package com.sweetzpot.stravazpot.activity.model;
 
 public enum PhotoSource {
-    STRAVA,
-    INSTAGRAM
+    STRAVA(1),
+    INSTAGRAM(2);
+
+    private int rawValue;
+
+    PhotoSource(int rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    public int getRawValue() {
+        return rawValue;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(rawValue);
+    }
 }
---
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/PhotoSourceTypeAdapter.java
@@ -0,0 +1,29 @@
+package com.sweetzpot.stravazpot.common.typeadapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.sweetzpot.stravazpot.activity.model.PhotoSource;
+
+import java.io.IOException;
+
+public class PhotoSourceTypeAdapter extends TypeAdapter<PhotoSource> {
+
+    @Override
+    public void write(JsonWriter out, PhotoSource photoSource) throws IOException {
+        out.value(photoSource.getRawValue());
+    }
+
+    @Override
+    public PhotoSource read(JsonReader in) throws IOException {
+        int input = in.nextInt();
+
+        for(PhotoSource photoSource : PhotoSource.values()) {
+            if(photoSource.getRawValue() == input) {
+                return photoSource;
+            }
+        }
+
+        return null;
+    }
+}

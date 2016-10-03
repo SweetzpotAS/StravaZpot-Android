610f680da0ee086355cf0a9c8012ae23d3bb8879
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-03 13:34:13 +0200

    Test a bad configuration scenario

--- lib/src/test/java/com/sweetzpot/stravazpot/activity/api/ExceptionsTest.java
+++ lib/src/test/java/com/sweetzpot/stravazpot/activity/api/ExceptionsTest.java
@@ -1,6 +1,8 @@
 package com.sweetzpot.stravazpot.activity.api;
 
 import com.sweetzpot.stravazpot.common.api.StravaAPITest;
+import com.sweetzpot.stravazpot.common.api.StravaConfig;
+import com.sweetzpot.stravazpot.common.api.exception.StravaAPIException;
 import com.sweetzpot.stravazpot.common.api.exception.StravaUnauthorizedException;
 
 import org.junit.Rule;
@@ -22,9 +24,29 @@ public class ExceptionsTest extends StravaAPITest{
         activityAPI.getActivity(123456).execute();
     }
 
+    @Test
+    public void shoudlThrowAnExceptionIfConfigurationIsNotValid() throws Exception {
+        enqueueResponse("{}");
+        ActivityAPI activityAPI = givenAWronglyConfiguredActivityAPI();
+
+        thrown.expect(StravaAPIException.class);
+
+        activityAPI.getActivity(123456).execute();
+    }
+
+    private ActivityAPI givenAWronglyConfiguredActivityAPI() {
+        return new ActivityAPI(givenAWrongConfig());
+    }
+
     private ActivityAPI givenAnActivityAPI() {
         return new ActivityAPI(givenAValidConfig());
     }
 
+    private StravaConfig givenAWrongConfig() {
+        return StravaConfig.withToken("A token")
+                            .baseURL("http://fake.strava.com/api/")
+                            .debug()
+                            .build();
+    }
 
 }

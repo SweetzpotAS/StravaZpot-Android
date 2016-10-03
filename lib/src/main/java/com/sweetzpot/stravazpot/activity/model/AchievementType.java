f2420b6d88ffde0b2f85a33afb3e1da5e4e581d5
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-03 13:25:19 +0200

    Create type adapter for achievement type

--- lib/src/main/java/com/sweetzpot/stravazpot/activity/model/AchievementType.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/activity/model/AchievementType.java
@@ -1,7 +1,17 @@
 package com.sweetzpot.stravazpot.activity.model;
 
 public enum AchievementType {
-    OVERALL,
-    PR,
-    YEAR_OVERALL
+    OVERALL(2),
+    PR(3),
+    YEAR_OVERALL(5);
+
+    private int rawValue;
+
+    AchievementType(int rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    public int getRawValue() {
+        return rawValue;
+    }
 }

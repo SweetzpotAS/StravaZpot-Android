b94198b1e2dedf68ab41b6ec8c362dd18bd7369e
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-04 19:31:20 +0200

    Create request to obtain a token

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/api/AuthenticationAPI.java
@@ -0,0 +1,19 @@
+package com.sweetzpot.stravazpot.authenticaton.api;
+
+import com.sweetzpot.stravazpot.authenticaton.model.AppCredentials;
+import com.sweetzpot.stravazpot.authenticaton.request.AuthenticationRequest;
+import com.sweetzpot.stravazpot.authenticaton.rest.AuthenticationRest;
+import com.sweetzpot.stravazpot.common.api.Config;
+import com.sweetzpot.stravazpot.common.api.StravaAPI;
+
+public class AuthenticationAPI extends StravaAPI{
+
+    public AuthenticationAPI(Config config) {
+        super(config);
+    }
+
+    public AuthenticationRequest getTokenForApp(AppCredentials appCredentials) {
+        return new AuthenticationRequest(appCredentials, getAPI(AuthenticationRest.class), this);
+    }
+
+}

22f680bfa0fba6342dfd02d84d5f1ee1f29d3fed
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-04 18:48:32 +0200

    Create interface to interact with authentication REST API

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/rest/AuthenticationRest.java
@@ -0,0 +1,18 @@
+package com.sweetzpot.stravazpot.authenticaton.rest;
+
+import com.sweetzpot.stravazpot.authenticaton.model.LoginResult;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface AuthenticationRest {
+
+    @POST("/oauth/token")
+    @FormUrlEncoded
+    Call<LoginResult> token(
+            @Field("client_id") int clientID,
+            @Field("client_secret") String clientSecret,
+            @Field("code") String code);
+}

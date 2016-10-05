1b5b85f2004c5a8599700e3d65bc1ee29c62c182
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-05 09:57:49 +0200

    Create custom view for Strava login button

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/ui/StravaLoginButton.java
@@ -0,0 +1,41 @@
+package com.sweetzpot.stravazpot.authenticaton.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+
+import com.sweetzpot.stravazpot.R;
+
+public class StravaLoginButton extends ImageButton {
+    public StravaLoginButton(Context context) {
+        this(context, null);
+    }
+
+    public StravaLoginButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public StravaLoginButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        int imageResource = R.drawable.btn_strava_connectwith_light;
+        if(attrs != null) {
+            TypedArray a = context.getTheme().obtainStyledAttributes(
+                    attrs,
+                    R.styleable.StravaLoginButton,
+                    0, 0);
+
+            try {
+                imageResource = a.getInteger(R.styleable.StravaLoginButton_type, 0) == 0 ?
+                                    R.drawable.btn_strava_connectwith_light :
+                                    R.drawable.btn_strava_connectwith_orange;
+            } finally {
+                a.recycle();
+            }
+        }
+
+        this.setImageResource(imageResource);
+    }
+
+}

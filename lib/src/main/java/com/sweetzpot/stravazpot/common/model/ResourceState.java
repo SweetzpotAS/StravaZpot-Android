fc677d3933e11da416f62cfb8c3723fe9251b18b
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-06 13:20:13 +0200

    Refactor resource state parsing

--- lib/src/main/java/com/sweetzpot/stravazpot/common/model/ResourceState.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/model/ResourceState.java
@@ -1,5 +1,20 @@
 package com.sweetzpot.stravazpot.common.model;
 
 public enum ResourceState {
-    META, SUMMARY, DETAILED
+    META(1), SUMMARY(2), DETAILED(3);
+
+    private int rawValue;
+
+    ResourceState(int rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    public int getRawValue() {
+        return rawValue;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(rawValue);
+    }
 }
--- lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/ResourceStateTypeAdapter.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/ResourceStateTypeAdapter.java
@@ -2,6 +2,7 @@ package com.sweetzpot.stravazpot.common.typeadapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.sweetzpot.stravazpot.common.model.ResourceState;
 
@@ -10,31 +11,22 @@ import java.io.IOException;
 public class ResourceStateTypeAdapter extends TypeAdapter<ResourceState> {
 
     @Override
-    public void write(JsonWriter out, ResourceState value) throws IOException {
-        switch (value) {
-            case SUMMARY:
-                out.value(2);
-                break;
-            case DETAILED:
-                out.value(3);
-                break;
-            case META:
-            default:
-                out.value(1);
-                break;
-        }
+    public void write(JsonWriter out, ResourceState resourceState) throws IOException {
+        out.value(resourceState.getRawValue());
     }
 
     @Override
     public ResourceState read(JsonReader in) throws IOException {
-        int value = in.nextInt();
-        if(value == 1) {
-            return ResourceState.META;
-        } else if(value == 2) {
-            return ResourceState.SUMMARY;
-        } else if(value == 3) {
-            return ResourceState.DETAILED;
+        if(!in.peek().equals(JsonToken.NULL)) {
+            int value = in.nextInt();
+            for(ResourceState resourceState : ResourceState.values()) {
+                if(resourceState.getRawValue() == value) {
+                    return resourceState;
+                }
+            }
+        } else {
+            in.nextNull();
         }
-        return ResourceState.META;
+        return null;
     }
 }

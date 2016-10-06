86104e446bdd2806a45029a038da6eb2ad92cf76
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-06 13:36:32 +0200

    Fix token parsing to avoid parsing

--- lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/TokenTypeAdapter.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/TokenTypeAdapter.java
@@ -2,6 +2,7 @@ package com.sweetzpot.stravazpot.common.typeadapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.sweetzpot.stravazpot.authenticaton.model.Token;
 
@@ -16,6 +17,11 @@ public class TokenTypeAdapter extends TypeAdapter<Token> {
 
     @Override
     public Token read(JsonReader in) throws IOException {
-        return new Token(in.nextString());
+        if(!in.peek().equals(JsonToken.NULL)) {
+            return new Token(in.nextString());
+        } else {
+            in.nextNull();
+            return null;
+        }
     }
 }

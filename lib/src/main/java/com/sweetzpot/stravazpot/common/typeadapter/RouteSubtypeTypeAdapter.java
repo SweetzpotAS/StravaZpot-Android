114d4dd98c45debc15459b423f049a625abb0ff6
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-06 13:23:06 +0200

    Refactor route subtype parsing

--- lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/RouteSubtypeTypeAdapter.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/RouteSubtypeTypeAdapter.java
@@ -2,6 +2,7 @@ package com.sweetzpot.stravazpot.common.typeadapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.sweetzpot.stravazpot.route.model.RouteSubtype;
 
@@ -10,42 +11,23 @@ import java.io.IOException;
 public class RouteSubtypeTypeAdapter extends TypeAdapter<RouteSubtype> {
 
     @Override
-    public void write(JsonWriter out, RouteSubtype value) throws IOException {
-        switch (value) {
-            case MTB:
-                out.value(2);
-                break;
-            case CX:
-                out.value(3);
-                break;
-            case TRAIL:
-                out.value(4);
-                break;
-            case MIXED:
-                out.value(5);
-                break;
-            case ROAD:
-            default:
-                out.value(1);
-        }
+    public void write(JsonWriter out, RouteSubtype routeSubtype) throws IOException {
+        out.value(routeSubtype.getRawValue());
     }
 
     @Override
     public RouteSubtype read(JsonReader in) throws IOException {
-        int value = in.nextInt();
+        if(!in.peek().equals(JsonToken.NULL)) {
+            int value = in.nextInt();
 
-        if(value == 1) {
-            return RouteSubtype.ROAD;
-        } else if(value == 2) {
-            return RouteSubtype.MTB;
-        } else if(value == 3) {
-            return RouteSubtype.CX;
-        } else if(value == 4) {
-            return RouteSubtype.TRAIL;
-        } else if(value == 5) {
-            return RouteSubtype.MIXED;
+            for(RouteSubtype routeSubtype : RouteSubtype.values()) {
+                if(routeSubtype.getRawValue() == value) {
+                    return routeSubtype;
+                }
+            }
+        } else {
+            in.nextNull();
         }
-
-        return RouteSubtype.ROAD;
+        return null;
     }
 }

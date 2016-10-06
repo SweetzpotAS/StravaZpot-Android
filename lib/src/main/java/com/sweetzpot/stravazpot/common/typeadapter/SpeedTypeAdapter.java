3e398211291f2d9f730e75804ae7505013e86c64
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-06 13:32:43 +0200

    Fix speed type parsing to avoid null

--- lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/SpeedTypeAdapter.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/SpeedTypeAdapter.java
@@ -2,6 +2,7 @@ package com.sweetzpot.stravazpot.common.typeadapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.sweetzpot.stravazpot.common.model.Speed;
 
@@ -16,6 +17,11 @@ public class SpeedTypeAdapter extends TypeAdapter<Speed> {
 
     @Override
     public Speed read(JsonReader in) throws IOException {
-        return Speed.metersPerSecond((float) (in.nextDouble()));
+        if(!in.peek().equals(JsonToken.NULL)) {
+            return Speed.metersPerSecond((float) (in.nextDouble()));
+        } else {
+            in.nextNull();
+            return null;
+        }
     }
 }

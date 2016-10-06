f4a611069b44fa61d1f6ee2dc44b96ce90912986
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-06 13:25:34 +0200

    Refactor route type parsing

--- lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/RouteTypeTypeAdapter.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/RouteTypeTypeAdapter.java
@@ -2,6 +2,7 @@ package com.sweetzpot.stravazpot.common.typeadapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.sweetzpot.stravazpot.route.model.RouteType;
 
@@ -10,28 +11,23 @@ import java.io.IOException;
 public class RouteTypeTypeAdapter extends TypeAdapter<RouteType> {
 
     @Override
-    public void write(JsonWriter out, RouteType value) throws IOException {
-        switch (value) {
-            case RUN:
-                out.value(2);
-                break;
-            case RIDE:
-            default:
-                out.value(1);
-                break;
-        }
+    public void write(JsonWriter out, RouteType routeType) throws IOException {
+        out.value(routeType.getRawValue());
     }
 
     @Override
     public RouteType read(JsonReader in) throws IOException {
-        int value = in.nextInt();
+        if(!in.peek().equals(JsonToken.NULL)) {
+            int value = in.nextInt();
 
-        if(value == 1) {
-            return RouteType.RIDE;
-        } else if(value == 2) {
-            return RouteType.RUN;
+            for(RouteType routeType : RouteType.values()) {
+                if(routeType.getRawValue() == value) {
+                    return routeType;
+                }
+            }
+        } else {
+            in.nextNull();
         }
-
-        return RouteType.RIDE;
+        return null;
     }
 }
--- lib/src/main/java/com/sweetzpot/stravazpot/route/model/RouteType.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/route/model/RouteType.java
@@ -1,6 +1,21 @@
 package com.sweetzpot.stravazpot.route.model;
 
 public enum RouteType {
-    RIDE,
-    RUN
+    RIDE(1),
+    RUN(2);
+
+    private int rawValue;
+
+    RouteType(int rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    public int getRawValue() {
+        return rawValue;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(rawValue);
+    }
 }

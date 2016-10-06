4c0bf931716768626b41b1599b8125ce1854b8c3
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-06 13:11:43 +0200

    Refactor friend status parsing

--- lib/src/main/java/com/sweetzpot/stravazpot/athlete/model/FriendStatus.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/athlete/model/FriendStatus.java
@@ -1,5 +1,16 @@
 package com.sweetzpot.stravazpot.athlete.model;
 
 public enum FriendStatus {
-    PENDING, ACCEPTED, BLOCKED, NOT_FRIENDS
+    PENDING("pending"), ACCEPTED("accepted"), BLOCKED("blocked"), NOT_FRIENDS("null");
+
+    private String rawValue;
+
+    FriendStatus(String rawValue) {
+        this.rawValue = rawValue;
+    }
+
+    @Override
+    public String toString() {
+        return rawValue;
+    }
 }

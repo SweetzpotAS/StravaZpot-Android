e032157a1b87fc97bed706856be953127652118f
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-06 13:09:22 +0200

    Refactor frame type parsing

--- lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/FrameTypeTypeAdapter.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/typeadapter/FrameTypeTypeAdapter.java
@@ -2,6 +2,7 @@ package com.sweetzpot.stravazpot.common.typeadapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.sweetzpot.stravazpot.gear.model.FrameType;
 
@@ -10,38 +11,22 @@ import java.io.IOException;
 public class FrameTypeTypeAdapter extends TypeAdapter<FrameType> {
 
     @Override
-    public void write(JsonWriter out, FrameType value) throws IOException {
-        switch (value) {
-            case CROSS:
-                out.value(2);
-                break;
-            case ROAD:
-                out.value(3);
-                break;
-            case TIME_TRIAL:
-                out.value(4);
-                break;
-            case MTB:
-            default:
-                out.value(1);
-                break;
-        }
+    public void write(JsonWriter out, FrameType frameType) throws IOException {
+        out.value(frameType.getRawValue());
     }
 
     @Override
     public FrameType read(JsonReader in) throws IOException {
-        int value = in.nextInt();
+        if(!in.peek().equals(JsonToken.NULL)) {
+            int value = in.nextInt();
 
-        if(value == 1) {
-            return FrameType.MTB;
-        } else if(value == 2) {
-            return FrameType.CROSS;
-        } else if(value == 3) {
-            return FrameType.ROAD;
-        } else if(value == 4) {
-            return FrameType.TIME_TRIAL;
+            for(FrameType type : FrameType.values()) {
+                if(type.getRawValue() == value) {
+                    return type;
+                }
+            }
         }
 
-        return FrameType.MTB;
+        return null;
     }
 }

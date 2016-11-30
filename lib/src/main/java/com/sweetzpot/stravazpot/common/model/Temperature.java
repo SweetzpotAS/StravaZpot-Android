52f61ed7dab69ca00844c1c942146e52874b2bb7
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-11-30 10:03:03 +0100

    Add named constructor and implement equals

--- lib/src/main/java/com/sweetzpot/stravazpot/common/model/Temperature.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/common/model/Temperature.java
@@ -1,9 +1,34 @@
 package com.sweetzpot.stravazpot.common.model;
 
 public class Temperature {
+
+    public static Temperature celsiusDegrees(float celsiusDegrees) {
+        return new Temperature(celsiusDegrees);
+    }
+
     private float celsiusDegrees;
+
+    private Temperature(float celsiusDegrees) {
+        this.celsiusDegrees = celsiusDegrees;
+    }
 
     public float getCelsiusDegrees() {
         return celsiusDegrees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Temperature that = (Temperature) o;
+
+        return Float.compare(that.celsiusDegrees, celsiusDegrees) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (celsiusDegrees != +0.0f ? Float.floatToIntBits(celsiusDegrees) : 0);
     }
 }

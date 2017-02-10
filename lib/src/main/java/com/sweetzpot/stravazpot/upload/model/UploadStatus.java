763db9185c7280ee3675393a86807cbe8a88f07f
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-02-10 14:09:44 +0100

    Create class to represent upload status

---
+++ lib/src/main/java/com/sweetzpot/stravazpot/upload/model/UploadStatus.java
@@ -0,0 +1,32 @@
+package com.sweetzpot.stravazpot.upload.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class UploadStatus {
+
+    @SerializedName("id") private int id;
+    @SerializedName("external_id") private String externalID;
+    @SerializedName("error") private String error;
+    @SerializedName("status") private String status;
+    @SerializedName("activity_id") private Integer activityID;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getExternalID() {
+        return externalID;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Integer getActivityID() {
+        return activityID;
+    }
+}
